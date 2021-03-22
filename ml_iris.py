805425174cb149a55e0e5f9ecfbafc107fd349f5
Sandra <sandra.luhtaniemi@gmail.com>
2021-03-22 20:26:03 +0200

    lisätty juttuja

--- ml_iris.py
+++ ml_iris.py
@@ -2,3 +2,8 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
+
+iris_dataset = load_iris()
+print("Target names: {}".format(iris_dataset['target_names']))
+print("Feature names: {}".format(iris_dataset['feature_names']))
+print("Type of data: {}".format(iris_dataset['data']))
