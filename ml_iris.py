2052cb22ad7e023967109cd7c713407ac04067cb
Sandra <sandra.luhtaniemi@gmail.com>
2021-03-23 18:01:09 +0200

    Lisää juttuja

--- ml_iris.py
+++ ml_iris.py
@@ -7,3 +7,8 @@ iris_dataset = load_iris()
 print("Target names: {}".format(iris_dataset['target_names']))
 print("Feature names: {}".format(iris_dataset['feature_names']))
 print("Type of data: {}".format(iris_dataset['data']))
+print("Shape of data: {}".format(iris_dataset['data'].shape))
+
+print("Type of target: {}".format(type(iris_dataset['target'])))
+print("Shape of target: {}".format(iris_dataset['target'].shape))
+print("Target:\n{}".format(iris_dataset['target']))
