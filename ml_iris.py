e812cf3a0cae81c54d7a9bcbe0d6c5dfe41c90db
Sandra <sandra.luhtaniemi@gmail.com>
2021-04-25 09:39:59 +0300

    lisätty juttuja

--- ml_iris.py
+++ ml_iris.py
@@ -13,6 +13,9 @@ print("Type of target: {}".format(type(iris_dataset['target'])))
 print("Shape of target: {}".format(iris_dataset['target'].shape))
 print("Target:\n{}".format(iris_dataset['target']))
 
-X_train, X_test, y_train, x_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
 print("X_train shape: {}".format(X_train.shape))
 print("y_train shape: {}".format(y_train.shape))
+
+print("X_test shape: {}".format(X_test.shape))
+print("y_test shape: {}".format(y_test.shape))
